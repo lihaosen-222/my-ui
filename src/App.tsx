70712c1f0eb2540ca55abf71bbace460fb1b598e
lihaosen-222 <59792563+lihaosen-222@users.noreply.github.com>
2022-07-12 09:45:40 +0800

    feat: 样式完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,19 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+// import 'antd/dist/antd.css'
+import  './App.less'
+import { Button, Card } from 'antd'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Card title="掘金签到" className='signIn'>今日已签到</Card>
+      <Card title="界面热键" className='winHotKey'>
+        1：绑定 谷歌浏览器<br/>
+        2：绑定 vscode<br/>
+        3：绑定 oneNote<br/>
+        4：未绑定<br/>
+        5：未绑定<br/>
+      </Card>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import vitePluginImp from "vite-plugin-imp";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    vitePluginImp({ // 按需导入 antd 样式
+      libList: [
+        {
+          libName: "antd",
+          style: (name) => `antd/lib/${name}/style/index.less`,
+        },
+      ],
+    }),
+  ],
+  css: {  
+    preprocessorOptions: {
+      less: { // 导入 less，得安装 less 包
+        javascriptEnabled: true,
+      },
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3001/',
+        changeOrigin: true,
+      },
+    }
+  }
+});
